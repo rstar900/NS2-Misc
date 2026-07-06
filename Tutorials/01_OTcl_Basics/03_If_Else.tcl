415d98e05ee55b8acb347fc80eaf96b610f1c174
Rachit Garg <rachitgarg900@gmail.com>
2026-07-06 19:14:23 +0200

    Create 03_If_Else.tcl with if-else example
    Add example of if-else statement in TCL.

---
+++ Tutorials/01_OTcl_Basics/03_If_Else.tcl
@@ -0,0 +1,10 @@
+set a 11
+
+# two ways to do if, but brace stays on the same line as if or else
+if {$a > 10} {
+puts "a is greater than 10"
+} else {
+puts "a is less than or equal to 10"
+}
+
+# Other way by if [expr $a > 10] { ... }
