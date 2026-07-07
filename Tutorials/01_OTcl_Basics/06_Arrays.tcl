916fd79901e95beb60442ec96444990b4f0bdb1f
Rachit Garg <rachitgarg900@gmail.com>
2026-07-07 18:01:18 +0200

    Add tutorial on indexed and associative arrays in TCL

---
+++ Tutorials/01_OTcl_Basics/06_Arrays.tcl
@@ -0,0 +1,21 @@
+# Indexed array
+puts "----------------Indexed--------------------"
+set a(0) 1
+set a(1) 2.67
+set a(2) "Hei!"
+
+puts "a(0) = $a(0)"
+puts "a(1) = $a(1)"
+puts "a(2) = $a(2)"
+puts "--------------------------------------------\n"
+
+# Indexed array
+puts "----------------Indexed--------------------"
+set b("John") 1
+set b("Wick") 2.67
+set b("Raj") "Hei!"
+
+puts "b(\"John\") = $b("John")"
+puts "b(\"Wick\") = $b("Wick")"
+puts "b(\"Raj\") = $b("Raj")"
+puts "--------------------------------------------\n"
